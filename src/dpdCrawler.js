69fdb9ab313a3fc940a8fba898950c8e0e8d9b04
mfurious <m.mestebeld@outlook.com>
2020-06-30 13:31:23 +0200

    make it better

---
+++ src/dpdCrawler.js
@@ -0,0 +1,71 @@
+const puppeteer = require("puppeteer");
+
+getTrackAndTrace = async (orderNumber) => {
+  const dpdURL = "https://dpdshippingreport.nl/";
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--proxy-server="direct://"', "--proxy-bypass-list=*"],
+  });
+  const page = await browser.newPage();
+
+  await page.goto(dpdURL, {
+    waitUntil: "networkidle0",
+  });
+
+  await page.type("#loginform > input[type=text]:nth-child(3)", "Ferm1234");
+  await page.type(
+    "#loginform > input[type=password]:nth-child(8)",
+    "8003CD113"
+  );
+
+  await Promise.all([await page.click("#loginform > button")]);
+
+  await page.goto(`${dpdURL}/deliverystatus.php`);
+  var input = await page.$("#from");
+  await input.click({ clickCount: 3 });
+  await input.type("2020-06-01");
+  await page.keyboard.press("Tab");
+  var input = await page.$("#till");
+  await input.type("2020-06-27");
+  await page.keyboard.press("Tab");
+  await page.type(
+    "#taskHistory > div > form > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(6) > input",
+    orderNumber
+  );
+  await page.keyboard.press("Enter");
+  await page.waitForNavigation({
+    waitUntil: "networkidle0",
+  });
+
+  var data = await page.evaluate(() => {
+    if (
+      !document.querySelector(
+        "#taskHistory > div > div:nth-child(7) > table > tbody > tr > td > table > tbody > tr > td:nth-child(3) > a"
+      )
+    ) {
+      data = { response: "No orders have been found" };
+      return data;
+    } else {
+      trackAndTraceCode = document.querySelector(
+        "#taskHistory > div > div:nth-child(7) > table > tbody > tr > td > table > tbody > tr > td:nth-child(3) > a"
+      ).innerHTML;
+
+      trackAndTraceStatus = document.querySelector(
+        "#taskHistory > div > div:nth-child(7) > table > tbody > tr > td > table > tbody > tr > td:nth-child(6)"
+      ).innerHTML;
+      trackAndTraceStatus = trackAndTraceStatus.split("&", 1);
+
+      return (data = {
+        response: {
+          trackAndTraceCode,
+          trackAndTraceStatus,
+        },
+      });
+    }
+  });
+  browser.close();
+  return data;
+};
+
+module.exports = { getTrackAndTrace };
---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const jwt = require('express-jwt');
+const jwksRsa = require("jwks-rsa")
+const { getTrackAndTrace } = require("./dpdCrawler");
+
+const PORT = process.env.PORT || 8888;
+var app = express();
+
+//Middlewarez
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("combined"));
+
+const jwtCheck = jwt({
+  secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://cloresy.eu.auth0.com/.well-known/jwks.json'
+}),
+audience: 'https://dpCrawler.io',
+issuer: 'https://cloresy.eu.auth0.com/',
+algorithms: ['RS256']
+});
+
+app.use(jwtCheck)
+
+app.get("/api/v1/trace/:orderid", async (req, res, next) => {
+   var orderid = req.params.orderid
+   data = await getTrackAndTrace(orderid)
+   res.json(data)
+});
+
+// Creating an instance
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
